c852060a375174289ed284975f4383850cb732c0
Aluno <Aluno@10.85.198.40>
2024-06-04 17:06:44 -0300

    Commit project FormulasMatemáticas

---
+++ src/FormulaMatemáticas.java
@@ -0,0 +1,30 @@
+public class FormulaMatemáticas {
+    
+    
+    public static void main(String[] args) {
+        
+        double x = 10;
+        double y = 5;
+        double z = 3;
+        
+        double a = Math.sqrt(Math.pov(x,5) -6) / 4;
+        
+        double b = x * y - 6 * x;
+        double s = 0;
+        
+        double c = 0;
+        c = 4 * Math.pov(c, 7) / (Math.pov(s, 5, b)) - (Math.pov(s, 7)) * Math pov(x, 2);
+        
+        double d = Math.pov(x, 4) - (6 * x Math.pov(y, 3));
+        
+        double e = 1 / (y - 1) / (x - 2 * y);
+        
+        double f = 7 * (Math.pov(c, 7) * (5 - Math.pov(s, 7) * Math sqrt(3 * x) - 4));
+        System.out.println("a, a + " + a);
+        System.out.println("b, b + " + a);
+        System.out.println("a, b + " + a);
+        System.out.println("a, b + " + a);
+        System.out.println("a, b + " + a);
+        System.out.println("a, b + " + a);
+    }
+}
